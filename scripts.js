9702ece1cdddc96d5be7084dd35c6a40903173f3
Juninho Dias <80465649+juninhodia@users.noreply.github.com>
2023-08-29 16:19:25 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,59 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('#carousel img');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    showSlide(currentSlide);
+
+    setInterval(() => {
+        currentSlide = (currentSlide + 1) % slides.length;
+        showSlide(currentSlide);
+    }, 5000); // Troca a cada 5 segundos
+});
+
+let index = 0;
+const images = document.querySelectorAll(".carousel-images img");
+const totalImages = images.length;
+
+document.querySelector(".prev").addEventListener("click", () => {
+    index = (index - 1 + totalImages) % totalImages;
+    updateCarousel();
+});
+
+document.querySelector(".next").addEventListener("click", () => {
+    index = (index + 1) % totalImages;
+    updateCarousel();
+});
+
+function updateCarousel() {
+    const offset = -index * 100;
+    document.querySelector(".carousel-images").style.transform = `translateX(${offset}%)`;
+}
+
+// Contador para adicionar produto 
+document.querySelectorAll('.counter').forEach(counterDiv => {
+    const decrementBtn = counterDiv.querySelector('.decrement');
+    const incrementBtn = counterDiv.querySelector('.increment');
+    const numberSpan = counterDiv.querySelector('.number');
+    let currentValue = 0;
+
+    decrementBtn.addEventListener('click', () => {
+        if (currentValue > 0) {
+            currentValue--;
+            numberSpan.textContent = currentValue;
+        }
+    });
+
+    incrementBtn.addEventListener('click', () => {
+        currentValue++;
+        numberSpan.textContent = currentValue;
+    });
+});
+
+
+
